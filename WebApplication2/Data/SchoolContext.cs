caac873fb39e0fb5a24a0a7a77743bc6aba1ad46
Rosataker <pechin1219@gmail.com>
2018-01-25 13:19:31 +0800

    lession 1

---
+++ WebApplication2/Data/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        #region 指定資料庫的情況
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Course>().ToTable("Course");
+        //    modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        //    modelBuilder.Entity<Student>().ToTable("Student");
+        //}
+        #endregion
+    }
+}
